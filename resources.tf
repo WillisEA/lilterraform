899bb14388610dd6edbac93a7fd6c3673aaa8988
Clayson Willis <claysonwillis@gmail.com>
2019-09-29 08:36:18 -0400

    added security groups

--- resources.tf
+++ resources.tf
@@ -10,3 +10,30 @@ resource "aws_vpc" "lil-example-two" {
 	}
 	
 }
+
+resource "aws_subnet" "subnet1" {
+	cidr_block = "${cidrsubnet(aws_vpc.lil-example-two.cidr_block, 3, 1)}"
+	vpc_id = "${aws_vpc.lil-example-two.id}"
+	availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+	cidr_block = "${cidrsubnet(aws_vpc.lil-example-two.cidr_block, 3, 2)}"
+	vpc_id = "${aws_vpc.lil-example-two.id}"
+	availability_zone = "us-east-1b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+	vpc_id = "${aws_vpc.lil-example-two.id}"
+
+	ingress {
+		cidr_blocks = [
+			"${aws_vpc.lil-example-two.cidr_block}"
+		]
+
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+	}
+  
+}
